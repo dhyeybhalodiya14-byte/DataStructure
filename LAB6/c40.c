04a254029faa2735a244e309a848b62641fc409a
Dhyey Bhalodiya <dhyeybhalodiya14@gmail.com>
2026-06-28 18:51:17 +0530

    Add program to demonstrate struct usage and student details in LAB6

---
+++ LAB6/c40.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+
+struct Student {
+    int roll_no;
+    char name[50];
+    char branch[50];
+    int batch_no;
+};
+
+int main() {
+    struct Student s1;
+    struct Student *p = &s1;
+
+    p->roll_no = 101;
+    strcpy(p->name, "Alex Mercer");
+    strcpy(p->branch, "Computer Science");
+    p->batch_no = 2026;
+
+    printf("Student Details:\n");
+    printf("Roll No  : %d\n", p->roll_no);
+    printf("Name     : %s\n", p->name);
+    printf("Branch   : %s\n", p->branch);
+    printf("Batch No : %d\n", p->batch_no);
+
+    return 0;
+}
