4035afa3acec976bb1e647c5dd6865d4d6361c7f
Dhyey Bhalodiya <dhyeybhalodiya14@gmail.com>
2026-06-29 23:07:45 +0530

    Refactor palindrome check variable names for clarity

--- LAB6/b39.c
+++ LAB6/b39.c
@@ -4,24 +4,24 @@
 int main() {
     char str[100];
     char *start, *end;
-    int isPalindrome = 1;
+    int flag = 1;
 
     printf("Enter a word: ");
-    scanf("%99s", str);
+    scanf("%s", str);
 
     start = str;
     end = str + strlen(str) - 1;
 
     while (start < end) {
         if (*start != *end) {
-            isPalindrome = 0;
+            flag = 0;
             break;
         }
         start++;
         end--;
     }
 
-    if (isPalindrome) {
+    if (flag == 1) {
         printf("Given word is Palindrome\n");
     } else {
         printf("Given word is Not palindrome\n");
