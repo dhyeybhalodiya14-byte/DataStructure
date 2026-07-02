ec516e078e78f81b2d8ed0ba1dd903d336059e8b
Dhyey Bhalodiya <dhyeybhalodiya14@gmail.com>
2026-07-02 12:31:55 +0530

    Add new programs for array manipulation and string case conversion in LAB4

---
+++ LAB4/b27.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+void main(){
+    int n,a[n + 1],i = 0,index,val;
+
+    printf("Enter size of an aay(n) :");
+    scanf("%d",&n);
+
+    printf("Enter value :");
+    scanf("%d",&val);
+
+    for(i = 0; i < n ; i++){
+        printf("Enter element :");
+        scanf("%d",&a[i]);
+    }
+
+    index = n;
+    for(i = 0 ; i < n; i++){
+        if(a[i] == val){
+            index = i;
+            break;
+        }
+    }
+
+    if(index < n){
+        for(i = index ; i < n - 1; i++){
+            a[i] = a[i + 1];
+        }
+        n--;
+
+        for(i = 0; i < n ; i++){
+            printf("%d\n ,",a[i]);
+        }
+    }
+
+    else{
+        printf("Element not Found");
+    }
+}
---
+++ LAB4/c30.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<string.h>
+void main(){
+    char S[100];
+
+    printf("Enter String :");
+    scanf("%s",S[100]);
+
+    for(int i = 0; i < 100; i++){
+        if('a' <= S[i] && 'z' >= S[i]){
+            S[i] = S[i] - 32;
+        }
+        else if('A' <= S[i] && 'z' >= S[i]){
+            S[i] = S[i] + 32;
+        }
+    }
+
+    printf("%d",S[100]);
+}
